3033d13a431592befd654ac8a87593f7fb6e5ec1
cleavon <cleavon.design.com>
2025-02-07 00:06:02 +0100

    handle negative numbers and abc

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 def is_prime(n):
     if n < 2:
         return False
-    for i in range(2, int(n ** 0.5) + 1):
+    for i in range(2, int(abs(n) ** 0.5) + 1):
         if n % i == 0:
             return False
     return True
@@ -17,18 +17,18 @@ def is_prime(n):
 def is_perfect(n: int):
     if n < 2:
         return False
-    divisors = [i for i in range(1, n) if n % i == 0]
-    return sum(divisors) == n
+    divisors = [i for i in range(1, abs(n)) if n % i == 0]
+    return sum(divisors) == abs(n)
 
 
-def is_Armstrong(n: int):
-    digits = [int(d) for d in str(n)]
+def is_armstrong(n: int):
+    digits = [int(d) for d in str(abs(n))]
     length = len(digits)
-    return sum(d ** length for d in digits) == n
+    return sum(d ** length for d in digits) == abs(n)
 
 
 def digit_sum(n: int):
-    return sum(int(d) for d in str(n))
+    return sum(int(d) for d in str(abs(n)))
 
 
 def get_fun_fact(n: int) -> str:
@@ -40,25 +40,43 @@ def get_fun_fact(n: int) -> str:
 @app.get("/api/classify-number")
 def classify_number(number: int = Query(..., description="the number to classify")):
     try:
-        number = int(number)
+        number_int = int(number)
     except ValueError:
-        return {"number": str(number), "error": True}
+        raise HTTPException(
+            status_code=400,
+            detail={
+                "number" : number,
+                "error": True,
+                "message": "input must be a valid integer."
+
+            }
+        )
+    if number_int < 0:
+        return {
+            "number": number_int,
+            "is_prime": False,
+            "is_perfect": False,
+            "properties": ["negative"],
+            "digit_sum": digit_sum(number_int),
+            "fun_fact": get_fun_fact(number_int),
+        }
+
 
     properties = []
-    if is_Armstrong(number):
-        properties.append("Armstrong")
-    if number % 2 == 0:
+    if is_armstrong(number_int):
+        properties.append("armstrong")
+    if number_int % 2 == 0:
         properties.append("even")
     else:
         properties.append("odd")
 
     return {
-        "number": number,
-        "is_prime": is_prime(number),
-        "is_perfect": is_perfect(number),
+        "number": number_int,
+        "is_prime": is_prime(number_int),
+        "is_perfect": is_perfect(number_int),
         "properties": properties,
-        "digit_sum": digit_sum(number),
-        "fun_fact": get_fun_fact(number),
+        "digit_sum": digit_sum(number_int),
+        "fun_fact": get_fun_fact(number_int),
     }
 
 
